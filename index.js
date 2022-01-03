35422c97d780baa7bf88e77720fb6aa664e0be77
sijo <sijo@accubits.com>
2022-01-03 15:59:29 +0530

    new version

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const express = require("express");
 const JsonDB = require("node-json-db").JsonDB;
 const Config = require("node-json-db/dist/lib/JsonDBConfig").Config;
 const uuid = require("uuid");
-const speakeasy = require("speakeasy");
-const twofactor = require("node-2fa");
 const cors = require('cors');
 const app = express();
 
-// The second argument is used to tell the DB to save after each push
-// If you put false, you'll have to call the save() method.
-// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
-// The last argument is the separator. By default it's slash (/)
 var db = new JsonDB(new Config("myDataBase", true, false, "/"));
+const authenticator = require('authenticator');
 
 app.use(cors());
 app.use(express.json());
@@ -21,36 +16,47 @@ app.get("/api", (req, res) => {
   res.json({ message: "Welcome to the two factor authentication exmaple" });
 });
 
-app.post("/api/register-speakeasy", (req, res) => {
+app.post("/api/register", (req, res) => {
   const id = uuid.v4();
   try {
     const path = `/user/${id}`;
-    // Create temporary secret until it it verified
-    const temp_secret = speakeasy.generateSecret();
-    // Create user in the database
-    db.push(path, { id, secret: temp_secret });
-    // Send user id and base32 key to user
-    res.json({ id, secret: temp_secret });
+    const formattedKey = authenticator.generateKey();
+    db.push(path, { id, secret: formattedKey });
+    const url = authenticator.generateTotpUri(formattedKey, "radhika@mailinator.com", 'INTECH FINANCE SA', 'SHA1', 10, 60);
+    const formattedToken = authenticator.generateToken(formattedKey);
+    res.json({ id, secret: formattedKey, token: formattedToken, url });
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: "Error generating secret key" });
   }
 });
 
-app.post("/api/verify-speakeasy", (req, res) => {
-  const { userId, token } = req.body;
+app.post("/api/regenerate-token", (req, res) => {
+  const { userId} = req.body;
   try {
-    // Retrieve user from database
     const path = `/user/${userId}`;
     const user = db.getData(path);
-    const { base32: secret } = user.secret;
-    const verified = speakeasy.totp.verify({
-      secret,
-      encoding: "base32",
-      token,
-    });
+    const { secret } = user;
+    const formattedToken = authenticator.generateToken(secret);
+    if (formattedToken) {
+      res.json({ token: formattedToken});
+    } else {
+      res.json({ verified: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving user" });
+  }
+});
+
+app.post("/api/verify", (req, res) => {
+  const { userId, token } = req.body;
+  try {
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    const { secret } = user;
+    const verified = authenticator.verifyToken(secret, token);
     if (verified) {
-      // Update user data
       db.push(path, { id: userId, secret: user.secret });
       res.json({ verified: true });
     } else {
@@ -61,72 +67,6 @@ app.post("/api/verify-speakeasy", (req, res) => {
     res.status(500).json({ message: "Error retrieving user" });
   }
 });
-
-app.post("/api/validate-speakeasy", (req, res) => {
-  const { userId, token } = req.body;
-  try {
-    // Retrieve user from database
-    const path = `/user/${userId}`;
-    const user = db.getData(path);
-    const { base32: secret } = user.secret;
-    const tokenValidates = speakeasy.totp.verify({
-      secret,
-      encoding: "base32",
-      token,
-      window: 1,
-    });
-    if (tokenValidates) {
-      res.json({ validated: true });
-    } else {
-      res.json({ validated: false });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error retrieving user" });
-  }
-});
-
-app.post("/api/register-node2fa", (req, res) => {
-  const id = uuid.v4();
-  try {
-    const path = `/user/${id}`;
-    // Create temporary secret until it it verified
-    const temp_secret = twofactor.generateSecret({
-      name: "My Awesome App",
-      account: "johndoe",
-    });
-    // Create user in the database
-    db.push(path, { id, temp_secret });
-    // Send user id and base32 key to user
-    res.json({ id, secret: temp_secret });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Error generating secret key" });
-  }
-});
-
-app.post("/api/verify-node2fa", (req, res) => {
-  const { userId, token } = req.body;
-  try {
-    // Retrieve user from database
-    const path = `/user/${userId}`;
-    const user = db.getData(path);
-    console.log({ user });
-    const { secret } = user.temp_secret;
-    const verified = twofactor.verifyToken(secret, token);
-    if (verified) {
-      // Update user data
-      db.push(path, { id: userId, temp_secret: user.temp_secret });
-      res.json({ verified: true });
-    } else {
-      res.json({ verified: false });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error retrieving user" });
-  }
-});
-
 
 const port = 9000;
 
